3f2138283f3743ee59f350cd5271d040703eff4f
genta <bolero.02.everything@gmail.com>
2014-07-06 00:48:15 +0900

    rebuild

--- dist/list.js
+++ dist/list.js
@@ -719,229 +719,237 @@ module.exports = function(val){
 });
 require.register("list.js/index.js", function(exports, require, module){
 /*
-ListJS with beta 1.0.0
-By Jonny Strömberg (www.jonnystromberg.com, www.listjs.com)
-*/
-(function( window, undefined ) {
-"use strict";
+ ListJS with beta 1.0.0
+ By Jonny Strömberg (www.jonnystromberg.com, www.listjs.com)
+ */
+(function (window, undefined) {
+	"use strict";
 
-var document = window.document,
-    getByClass = require('get-by-class'),
-    extend = require('extend'),
-    indexOf = require('indexof');
+	var document = window.document,
+		getByClass = require('get-by-class'),
+		extend = require('extend'),
+		indexOf = require('indexof');
 
-var List = function(id, options, values) {
+	var List = function (id, options, values) {
 
-    var self = this,
-		init,
-        Item = require('./src/item')(self),
-        addAsync = require('./src/add-async')(self),
-        parse = require('./src/parse')(self);
+		var self = this,
+			init,
+			Item = require('./src/item')(self),
+			addAsync = require('./src/add-async')(self),
+			parse = require('./src/parse')(self);
 
-    init = {
-        start: function() {
-            self.listClass      = "list";
-            self.searchClass    = "search";
-            self.sortClass      = "sort";
-            self.page           = 200;
-            self.i              = 1;
-            self.items          = [];
-            self.visibleItems   = [];
-            self.matchingItems  = [];
-            self.searched       = false;
-            self.filtered       = false;
-            self.handlers       = { 'updated': [] };
-            self.plugins        = {};
-            self.helpers        = {
-                getByClass: getByClass,
-                extend: extend,
-                indexOf: indexOf
-            };
+		init = {
+			start: function () {
+				self.listClass = "list";
+				self.searchClass = "search";
+				self.sortClass = "sort";
+				self.page = 200;
+				self.i = 1;
+				self.items = [];
+				self.visibleItems = [];
+				self.matchingItems = [];
+				self.searched = false;
+				self.filtered = false;
+				self.handlers = { 'updated': [], 'itemShow' : [] };
+				self.plugins = {};
+				self.helpers = {
+					getByClass: getByClass,
+					extend: extend,
+					indexOf: indexOf
+				};
 
-            extend(self, options);
+				extend(self, options);
 
-            self.listContainer = (typeof(id) === 'string') ? document.getElementById(id) : id;
-            if (!self.listContainer) { return; }
-            self.list           = getByClass(self.listContainer, self.listClass, true);
+				self.listContainer = (typeof(id) === 'string') ? document.getElementById(id) : id;
+				if (!self.listContainer) {
+					return;
+				}
+				self.list = getByClass(self.listContainer, self.listClass, true);
 
-            self.templater      = require('./src/templater')(self);
-            self.search         = require('./src/search')(self);
-            self.filter         = require('./src/filter')(self);
-            self.sort           = require('./src/sort')(self);
+				self.templater = require('./src/templater')(self);
+				self.search = require('./src/search')(self);
+				self.filter = require('./src/filter')(self);
+				self.sort = require('./src/sort')(self);
 
-            this.items();
-            self.update();
-            this.plugins();
-        },
-        items: function() {
-            parse(self.list);
-            if (values !== undefined) {
-                self.add(values);
-            }
-        },
-        plugins: function() {
-            for (var i = 0; i < self.plugins.length; i++) {
-                var plugin = self.plugins[i];
-                self[plugin.name] = plugin;
-                plugin.init(self);
-            }
-        }
-    };
+				this.items();
+				self.update();
+				this.plugins();
+			},
+			items: function () {
+				parse(self.list);
+				if (values !== undefined) {
+					self.add(values);
+				}
+			},
+			plugins: function () {
+				for (var i = 0; i < self.plugins.length; i++) {
+					var plugin = self.plugins[i];
+					self[plugin.name] = plugin;
+					plugin.init(self);
+				}
+			}
+		};
 
 
-    /*
-    * Add object to list
-    */
-    this.add = function(values, callback) {
-        if (callback) {
-            addAsync(values, callback);
-            return;
-        }
-        var added = [],
-            notCreate = false;
-        if (values[0] === undefined){
-            values = [values];
-        }
-        for (var i = 0, il = values.length; i < il; i++) {
-            var item = null;
-            if (values[i] instanceof Item) {
-                item = values[i];
-                item.reload();
-            } else {
-                notCreate = (self.items.length > self.page) ? true : false;
-                item = new Item(values[i], undefined, notCreate);
-            }
-            self.items.push(item);
-            added.push(item);
-        }
-        self.update();
-        return added;
-    };
+		/*
+		 * Add object to list
+		 */
+		this.add = function (values, callback) {
+			if (callback) {
+				addAsync(values, callback);
+				return;
+			}
+			var added = [],
+				notCreate = false;
+			if (values[0] === undefined) {
+				values = [values];
+			}
+			for (var i = 0, il = values.length; i < il; i++) {
+				var item = null;
+				if (values[i] instanceof Item) {
+					item = values[i];
+					item.reload();
+				} else {
+					notCreate = (self.items.length > self.page) ? true : false;
+					item = new Item(values[i], undefined, notCreate);
+				}
+				self.items.push(item);
+				added.push(item);
+			}
+			self.update();
+			return added;
+		};
 
-	this.show = function(i, page) {
-		this.i = i;
-		this.page = page;
-		self.update();
-        return self;
+		this.show = function (i, page) {
+			this.i = i;
+			this.page = page;
+			self.update();
+			return self;
+		};
+
+		/* Removes object from list.
+		 * Loops through the list and removes objects where
+		 * property "valuename" === value
+		 */
+		this.remove = function (valueName, value, options) {
+			var found = 0;
+			for (var i = 0, il = self.items.length; i < il; i++) {
+				if (self.items[i].values()[valueName] == value) {
+					self.templater.remove(self.items[i], options);
+					self.items.splice(i, 1);
+					il--;
+					i--;
+					found++;
+				}
+			}
+			self.update();
+			return found;
+		};
+
+		/* Gets the objects in the list which
+		 * property "valueName" === value
+		 */
+		this.get = function (valueName, value) {
+			var matchedItems = [];
+			for (var i = 0, il = self.items.length; i < il; i++) {
+				var item = self.items[i];
+				if (item.values()[valueName] == value) {
+					matchedItems.push(item);
+				}
+			}
+			return matchedItems;
+		};
+
+		/*
+		 * Get size of the list
+		 */
+		this.size = function () {
+			return self.items.length;
+		};
+
+		/*
+		 * Removes all items from the list
+		 */
+		this.clear = function () {
+			self.templater.clear();
+			self.items = [];
+			return self;
+		};
+
+		this.on = function (event, callback) {
+			self.handlers[event].push(callback);
+			return self;
+		};
+
+		this.off = function (event, callback) {
+			var e = self.handlers[event];
+			var index = indexOf(e, callback);
+			if (index > -1) {
+				e.splice(index, 1);
+			}
+			return self;
+		};
+
+		this.trigger = function (event, opt) {
+			var i = self.handlers[event].length;
+			while (i--) {
+				self.handlers[event][i](self, opt);
+			}
+			return self;
+		};
+
+		this.reset = {
+			filter: function () {
+				var is = self.items,
+					il = is.length;
+				while (il--) {
+					is[il].filtered = false;
+				}
+				return self;
+			},
+			search: function () {
+				var is = self.items,
+					il = is.length;
+				while (il--) {
+					is[il].found = false;
+				}
+				return self;
+			}
+		};
+
+		this.update = function () {
+			var is = self.items,
+				il = is.length;
+
+			self.visibleItems = [];
+			self.matchingItems = [];
+			self.templater.clear();
+			var visibleIndex = 0;
+			for (var i = 0; i < il; i++) {
+				var isMatch = is[i].matching();
+				var isVisible = isMatch && ((self.matchingItems.length + 1) >= self.i && self.visibleItems.length < self.page);
+				is[i].listIndex = i;
+				is[i].listVisibleIndex = -1;
+				if (isMatch && isVisible) {
+					is[i].listVisibleIndex = visibleIndex++;
+					is[i].show();
+					self.visibleItems.push(is[i]);
+					self.matchingItems.push(is[i]);
+				} else if (isMatch) {
+					self.matchingItems.push(is[i]);
+					is[i].hide();
+				} else {
+					is[i].hide();
+				}
+			}
+			self.trigger('updated');
+			return self;
+		};
+
+		init.start();
 	};
 
-    /* Removes object from list.
-    * Loops through the list and removes objects where
-    * property "valuename" === value
-    */
-    this.remove = function(valueName, value, options) {
-        var found = 0;
-        for (var i = 0, il = self.items.length; i < il; i++) {
-            if (self.items[i].values()[valueName] == value) {
-                self.templater.remove(self.items[i], options);
-                self.items.splice(i,1);
-                il--;
-                i--;
-                found++;
-            }
-        }
-        self.update();
-        return found;
-    };
-
-    /* Gets the objects in the list which
-    * property "valueName" === value
-    */
-    this.get = function(valueName, value) {
-        var matchedItems = [];
-        for (var i = 0, il = self.items.length; i < il; i++) {
-            var item = self.items[i];
-            if (item.values()[valueName] == value) {
-                matchedItems.push(item);
-            }
-        }
-        return matchedItems;
-    };
-
-    /*
-    * Get size of the list
-    */
-    this.size = function() {
-        return self.items.length;
-    };
-
-    /*
-    * Removes all items from the list
-    */
-    this.clear = function() {
-        self.templater.clear();
-        self.items = [];
-        return self;
-    };
-
-    this.on = function(event, callback) {
-        self.handlers[event].push(callback);
-        return self;
-    };
-
-    this.off = function(event, callback) {
-        var e = self.handlers[event];
-        var index = indexOf(e, callback);
-        if (index > -1) {
-            e.splice(index, 1);
-        }
-        return self;
-    };
-
-    this.trigger = function(event) {
-        var i = self.handlers[event].length;
-        while(i--) {
-            self.handlers[event][i](self);
-        }
-        return self;
-    };
-
-    this.reset = {
-        filter: function() {
-            var is = self.items,
-                il = is.length;
-            while (il--) {
-                is[il].filtered = false;
-            }
-            return self;
-        },
-        search: function() {
-            var is = self.items,
-                il = is.length;
-            while (il--) {
-                is[il].found = false;
-            }
-            return self;
-        }
-    };
-
-    this.update = function() {
-        var is = self.items,
-			il = is.length;
-
-        self.visibleItems = [];
-        self.matchingItems = [];
-        self.templater.clear();
-        for (var i = 0; i < il; i++) {
-            if (is[i].matching() && ((self.matchingItems.length+1) >= self.i && self.visibleItems.length < self.page)) {
-                is[i].show();
-                self.visibleItems.push(is[i]);
-                self.matchingItems.push(is[i]);
-			} else if (is[i].matching()) {
-                self.matchingItems.push(is[i]);
-                is[i].hide();
-			} else {
-                is[i].hide();
-			}
-        }
-        self.trigger('updated');
-        return self;
-    };
-
-    init.start();
-};
-
-module.exports = List;
+	module.exports = List;
 
 })(window);
 
@@ -1173,59 +1181,62 @@ module.exports = function(list) {
 
 });
 require.register("list.js/src/item.js", function(exports, require, module){
-module.exports = function(list) {
-    return function(initValues, element, notCreate) {
-        var item = this;
+module.exports = function (list) {
+	return function (initValues, element, notCreate) {
+		var item = this;
 
-        this._values = {};
+		this._values = {};
 
-        this.found = false; // Show if list.searched == true and this.found == true
-        this.filtered = false;// Show if list.filtered == true and this.filtered == true
+		this.found = false; // Show if list.searched == true and this.found == true
+		this.filtered = false;// Show if list.filtered == true and this.filtered == true
 
-        var init = function(initValues, element, notCreate) {
-            if (element === undefined) {
-                if (notCreate) {
-                    item.values(initValues, notCreate);
-                } else {
-                    item.values(initValues);
-                }
-            } else {
-                item.elm = element;
-                var values = list.templater.get(item, initValues);
-                item.values(values);
-            }
-        };
-        this.values = function(newValues, notCreate) {
-            if (newValues !== undefined) {
-                for(var name in newValues) {
-                    item._values[name] = newValues[name];
-                }
-                if (notCreate !== true) {
-                    list.templater.set(item, item.values());
-                }
-            } else {
-                return item._values;
-            }
-        };
-        this.show = function() {
-            list.templater.show(item);
-        };
-        this.hide = function() {
-            list.templater.hide(item);
-        };
-        this.matching = function() {
-            return (
-                (list.filtered && list.searched && item.found && item.filtered) ||
-                (list.filtered && !list.searched && item.filtered) ||
-                (!list.filtered && list.searched && item.found) ||
-                (!list.filtered && !list.searched)
-            );
-        };
-        this.visible = function() {
-            return (item.elm.parentNode == list.list) ? true : false;
-        };
-        init(initValues, element, notCreate);
-    };
+		var init = function (initValues, element, notCreate) {
+			item.listIndex = -1;
+			item.visibleListIndex = -1;
+			if (element === undefined) {
+				if (notCreate) {
+					item.values(initValues, notCreate);
+				} else {
+					item.values(initValues);
+				}
+			} else {
+				item.elm = element;
+				var values = list.templater.get(item, initValues);
+				item.values(values);
+			}
+		};
+		this.values = function (newValues, notCreate) {
+			if (newValues !== undefined) {
+				for (var name in newValues) {
+					item._values[name] = newValues[name];
+				}
+				if (notCreate !== true) {
+					list.templater.set(item, item.values());
+				}
+			} else {
+				return item._values;
+			}
+		};
+		this.show = function () {
+			list.templater.show(item);
+			list.trigger('itemShow', item);
+		};
+		this.hide = function () {
+			list.templater.hide(item);
+		};
+		this.matching = function () {
+			return (
+				(list.filtered && list.searched && item.found && item.filtered) ||
+				(list.filtered && !list.searched && item.filtered) ||
+				(!list.filtered && list.searched && item.found) ||
+				(!list.filtered && !list.searched)
+				);
+		};
+		this.visible = function () {
+			return (item.elm.parentNode == list.list) ? true : false;
+		};
+		init(initValues, element, notCreate);
+	};
 };
 
 });
@@ -1280,7 +1291,7 @@ var Templater = function(list) {
                     // TODO speed up if possible
                     var elm = getByClass(item.elm, v, true);
                     if (elm) {
-												var val = fm ? fm(v, value[v]) : value[v];
+												var val = fm ? fm(v, values[v]) : values[v];
                         /* src attribute for image tag & text for other tags */
                         if (elm.tagName === "IMG" && val !== "") {
                             elm.src = val;
